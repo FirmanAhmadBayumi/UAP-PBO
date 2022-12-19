f5f685f5bc577c489f6c73e2be88c19607d32551
Yoel Marcel Siregar <93704152+YoelMarcelSiregar@users.noreply.github.com>
2022-12-19 11:56:42 +0700

    Update ProductCounter.java

--- UAP_PBO/src/Entity/ProductCounter.java
+++ UAP_PBO/src/Entity/ProductCounter.java
@@ -1,1 +1,13 @@
+package Entity;
+
+public interface ProductCounter {
+    double TAX = 0;
+
+    public double getTAX();
+
+    public void setTAX(double TAX);
+    
+    public void hitungJumlahProduk();
+    public void hitungHargaProduk();
+}
 
